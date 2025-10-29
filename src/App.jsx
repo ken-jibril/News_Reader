10ea24a2133b6bad334ae112acdfe047205e2992
ken kagai <jibken80@gmail.com>
2025-10-29 14:36:54 +0300

    update the heading styling to be centered and add some margin and increase its size

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
 
   return (
     <>
-       <h1 className="text-gray-700 px-2 py-1 text-3xl">News Reader</h1>
+       <h1 className="text-gray-700 text-center my-1 text-4xl">News Reader</h1>
     </>
   )
 }
