6070d87b9215fb371224275afb0925f8e39c2f0f
ken kagai <jibken80@gmail.com>
2025-10-29 14:33:09 +0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+       <h1 className="text-gray-700 px-2 py-1 text-3xl">News Reader</h1>
+    </>
+  )
+}
+
+export default App
