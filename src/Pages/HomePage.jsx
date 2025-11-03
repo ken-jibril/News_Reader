716f4f0a77f352f5efce9d5f34590f0b0573134d
ken kagai <jibken80@gmail.com>
2025-11-03 21:31:26 +0300

    Add a home page file and add an aside block for the heading and links

---
+++ src/Pages/HomePage.jsx
@@ -0,0 +1,14 @@
+function HomePage() {
+    return ( 
+        <div className="">
+            <aside className="w-1/5 bg-gray-700 text-white flex flex-col p-6 space-y-4">
+                <h1 className="text-3xl font-semibold border-b-2 border-white pb-2 ">DashBoard</h1>
+                <nav className="">
+
+                </nav>
+            </aside>
+        </div>
+     );
+}
+
+export default HomePage;
