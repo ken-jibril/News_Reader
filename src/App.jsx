1e8d4468c4077d57afe28c07ac7c300bbf0a543c
ken kagai <jibken80@gmail.com>
2025-11-05 19:50:29 +0300

    update the file to have use routes for navigating through the site.

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import { Route, Routes } from 'react-router-dom'
 import HomePage from './Pages/HomePage'
 
 
@@ -7,7 +8,9 @@ function App() {
 
   return (
     <>
-      <HomePage />
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+      </Routes>
     </>
   )
 }
